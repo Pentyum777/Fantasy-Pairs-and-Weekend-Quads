b79d42d94c406ad7c856968ea1132b227b1dc903
Wayne <wpenfold@bigpond.net.au>
2026-01-15 23:41:04 +1100

    Automated deploy: 2026-01-15 23:41:04

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -46,9 +46,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "35863569f5b4901c50cff2ed6176c69e
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "21beabf95bdb10d4d34d58f2a5734443",
-"/": "21beabf95bdb10d4d34d58f2a5734443",
-"main.dart.js": "f2b283c84934ab3788dfe09201a1d806",
+"index.html": "03759243e82a063e5a62ed58af448c29",
+"/": "03759243e82a063e5a62ed58af448c29",
+"main.dart.js": "31151e01a9929187308f7491daecc564",
 "manifest.json": "18b05de58f5aeed2ffa6be1246cc43f1",
 "msal.js": "048932bbbfef32d16327437ca5db5d6b",
 "version.json": "b0d2ea9d293a4ff4175362a67375b4a4"};
