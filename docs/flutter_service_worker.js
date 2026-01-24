03de6231312dfd584d06963b41cffb9e6e46c371
Wayne <wpenfold@bigpond.net.au>
2026-01-25 09:02:19 +1100

    Deploy

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -50,7 +50,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0250fe103a44fd8d5f55d2ca68be70c5
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ff5cb34dc688fa8e3e1cc12bc1a60d74",
 "/": "ff5cb34dc688fa8e3e1cc12bc1a60d74",
-"main.dart.js": "752c31b209dc5a7d11dc905edd5f8d35",
+"main.dart.js": "d4985d9fbded82c8b10c9680b133d996",
 "manifest.json": "18b05de58f5aeed2ffa6be1246cc43f1",
 "msal.js": "7dd4ad38d883c25e5652b5e7b00bbfe9",
 "version.json": "b0d2ea9d293a4ff4175362a67375b4a4"};
