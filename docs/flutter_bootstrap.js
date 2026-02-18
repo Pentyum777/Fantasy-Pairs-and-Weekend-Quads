a9817c1f7ef25348643853160bf2dab22a4913c0
Wayne <wpenfold@bigpond.net.au>
2026-02-18 11:05:31 +1100

    Source update

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1423465202"
+    serviceWorkerVersion: "2149684882"
   }
 });
