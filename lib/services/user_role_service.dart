1bc66617f726b725d1778e6130b374768e923bd8
Wayne <wpenfold@bigpond.net.au>
2026-03-12 23:09:21 +1100

    Source update

--- lib/services/user_role_service.dart
+++ lib/services/user_role_service.dart
@@ -6,8 +6,7 @@ class UserRoleService {
     "paulfruin30@gmail.com",
     "wayne.penfold@gmail.com",
     "wayneliz7@outlook.com",
-    "chrisoakenfall@gmail.com",
-  };
+     };
 
   String? _currentUser;
   UserRole role = UserRole.readOnly;
